7bf1cb1fdd5cd99513d66c894e2804e187dfe3a1
BilalThaha <bilal.athaha@gmail.com>
2024-09-05 22:54:41 +0530

    Reverse a linked list

--- src/main/java/com/dncoyote/DSA/blind75/linked_list/ReverseLinkedListOptimal.java
+++ src/main/java/com/dncoyote/DSA/blind75/linked_list/ReverseLinkedListOptimal.java
@@ -1,53 +1,5 @@
-# **Linked List**
-## **Theory**
+package com.dncoyote.DSA.blind75.linked_list;
 
-#### What is a Linked List
-- In Array, values are stored in contiguous memory locations. Array is easy to traverse, but we cannot increase or decrease size.
-- This is where a Linked List comes into the picture.
-- Linked List is not stored in contiguous memory locations. They are stored in heap memory and their size can be increased and decreased anytime.
-- With every data, the `next` value is stored.
-- Starting point of Linked List is called as `head`.
-- The `next` value of the last element(also called as `tail`)  is `null`.
-- When a new value is to be stored
-    - Add the value somewhere in the heap.
-    - Update the `next` value of last element to the location of the new value.
-
-#### Where is it used?
-- In Data Structures, Linked List is used in Stacks and Queue.
-- In real life, we use it in browser.
-
-#### Struct/Class in C++ Java
-```c++
-int x =2;
-int* y = &x;
-cout<<y;
-```
-- Creates `x` somewhere in the heap memory and stores 2.
-- Stores the memory location of `x` in `y`.
-
-## **Reverse Linked List**
->
-#### Brute - 
->Time Complexity - 
-
->Space Complexity - 
-```java
-
-```
-#### Explanation
-
--
-
-#### Steps
-
--
-
-#### Optimal -
->Time Complexity - 
-
->Space Complexity - 
-
-```java
 class ListNode {
     int val;
     ListNode next;
@@ -102,11 +54,3 @@ public class ReverseLinkedListOptimal {
         solution.printList(reversedHead);
     }
 }
-```
-#### Explanation
-
--
-
-#### Steps
-
--
