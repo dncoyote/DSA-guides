79faace233ec916160f3c9d0156f60b6b46042e4
dncoyote <bilal.athaha@gmail.com>
2025-07-02 21:03:27 +0530

    test

--- dsa/binarytree/KthSmallestElementInBST.java
+++ dsa/binarytree/KthSmallestElementInBST.java
@@ -16,23 +16,24 @@ public class KthSmallestElementInBST {
   }
 
   private static int kthSmallest(TreeNode root, int k) {
-    int count =  0 ;
-    int result =  -1 ;
+    int[] count = { 0 };
+    int[] result = { -1 };
 
     inorder(root, count, result, k);
 
-    return result;
+    return result[0];
   }
 
-  private static void inorder(TreeNode root, int count, int result, int k) {
+  private static void inorder(TreeNode root, int[] count, int[] result, int k) {
     if (root == null)
       return;
     inorder(root.left, count, result, k);
-    count++;
-    if (count == k) {
-      result = root.val;
+    count[0]++;
+    if (count[0] == k) {
+      result[0] = root.val;
       return;
     }
     inorder(root.right, count, result, k);
   }
+
 }
